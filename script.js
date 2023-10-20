d0c75b817aab7213cab66c624323dd955a1c116c
Amit Chahar <amitchahar9837@gmail.com>
2023-10-20 15:08:27 +0530

    this is second commit for responsive

--- script.js
+++ script.js
@@ -12,21 +12,23 @@ let page=1;
 
 async function searchImages(){
     keyword=searchImgInput.value;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
-    const response= await fetch(url);
-    const data=await response.json();
-    console.log(data);
-    const results=data.results;
-    results.map((result)=>{
-        const image=document.createElement("img");
-        image.src=result.urls.small;
-        const imageLink=document.createElement("a");
-        imageLink.href=result.links.html;
-        imageLink.target="_blank";
-        imageLink.appendChild(image);
-        searchResult.appendChild(imageLink);
-    })
-    showMoreBtn.style.display="block";
+    if(keyword!=""){
+        const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+        const response= await fetch(url);
+        const data=await response.json();
+        console.log(data);
+        const results=data.results;
+        results.map((result)=>{
+            const image=document.createElement("img");
+            image.src=result.urls.small;
+            const imageLink=document.createElement("a");
+            imageLink.href=result.links.html;
+            imageLink.target="_blank";
+            imageLink.appendChild(image);
+            searchResult.appendChild(imageLink);
+        })
+        showMoreBtn.style.display="block";
+    }
 }
 
 showMoreBtn.addEventListener("click",()=>{
